3671a2f8c843a4806c7d9a154a96dadc4e3c7a5c
Rafael Garcia del Aguila <rafael.garcia@gft.com>
2016-07-06 15:45:38 +0200

    feat(*): adds changes made in the previous step

---
+++ webapp/src/app/list/defaults.ts
@@ -0,0 +1,14 @@
+import { Todo } from './todo';
+
+export const todos: Todo[] = [
+    {
+        title: 'My first todo',
+        description: 'This is placeholder TODO.',
+        completed: false
+    }, 
+    {
+        title: 'Develop AppComponent',
+        description: 'Complete the development of the layout of our Web App',
+        completed: true
+    }
+];
--- webapp/src/app/list/list.component.ts
+++ webapp/src/app/list/list.component.ts
@@ -1,13 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import { MD_CARD_DIRECTIVES } from '@angular2-material/card';
+import { Todo } from './todo';
+import { todos } from './defaults';
 
 @Component({
   moduleId: module.id,
   selector: 'app-list',
   templateUrl: 'list.component.html',
-  styleUrls: ['list.component.css']
+  styleUrls: ['list.component.css'],
+  directives: [
+    MD_CARD_DIRECTIVES
+  ]
 })
 export class ListComponent implements OnInit {
-
+  todos: Todo[] = todos;
   constructor() {}
 
   ngOnInit() {}
